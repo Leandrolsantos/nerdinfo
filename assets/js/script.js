7e3a9283205d0e9f3ff9f3fcfd1f97a9861380f8
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-11-20 21:04:43 -0100

    310º dia de CTD - HTML + CSS + JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 //window.alert('Bem vindo ao NerdInfo!')
 //window.prompt('Qual é seu nome?');
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 12 * i);
+    });
+  }
+
+  const texto = document.querySelector('p');
+  typeWriter(texto);
